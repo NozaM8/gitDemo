5665fe1804d303171dc68de17e3bfb934c8d69c3
NozaM8 <cutenora05@gmail.com>
2020-08-01 14:36:34 -0500

    add BookARoom.java

---
+++ src/BookARoom.java
@@ -0,0 +1,24 @@
+public class BookARoom {
+    public static void main(String[] args) {
+
+        System.out.println("Hey! I wanna book a room. Can you help me please?");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
