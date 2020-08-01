a939c52f2bbed7d1c7066221b84b87fcd1399389
NozaM8 <cutenora05@gmail.com>
2020-08-01 10:37:31 -0500

    add HappyB20.java

---
+++ src/HappyB20.java
@@ -0,0 +1,21 @@
+public class HappyB20 {
+    public static void main(String[] args) {
+
+        System.out.println("K, so here's the deal.");
+        System.out.println("Never underestimate yourself. You can do much more than you think you can.");
+        System.out.println("Believe in yourself!");
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
